044ac460f5f9fa29b791b8553a7c75abb1752f59
KDCastilloR <kcastillor13@miumg.edu.gt>
2025-06-01 17:51:09 -0600

    Update top_maquina.sv

--- src/top_maquina.sv
+++ src/top_maquina.sv
@@ -1,47 +1,34 @@
-module tt_um_KDCastilloR_FSMVENDING (
-    input  [7:0] ui,
-    output [7:0] uo,
-    inout  [7:0] uio
+`timescale 1ns / 1ps
+`default_nettype none
+
+module top_maquina (
+    input wire clk,
+    input wire rst,
+    input wire [1:0] moneda,
+    input wire [1:0] seleccion,
+    output wire [1:0] producto,
+    output wire listo,
+    output wire [1:0] cambio
 );
-    wire clk       = ui[0];
-    wire reset     = ui[1];
-    wire [1:0] moneda = ui[3:2];
-    wire comprarA  = ui[4];
-    wire comprarB  = ui[5];
 
-    wire listoA, listoB;
     wire [3:0] total;
-    wire [3:0] cambio;
 
-    wire vendA, vendB;
-
-    assign uo[0] = listoA;
-    assign uo[1] = listoB;
-    assign uo[5:2] = total; // bits 2 a 5 muestran el total acumulado
-    // uo[6], uo[7] libres
-
-    assign uio = 8'bz; // no se usan pines bidireccionales
-
-    moore_fsm moore_inst (
+    moore_fsm moore (
         .clk(clk),
-        .reset(reset),
+        .rst(rst),
         .moneda(moneda),
-        .comprarA(comprarA),
-        .comprarB(comprarB),
-        .listoA(listoA),
-        .listoB(listoB),
-        .total(total),
-        .vendA(vendA),
-        .vendB(vendB)
+        .total(total)
     );
 
-    mealy_fsm mealy_inst (
+    mealy_fsm mealy (
         .clk(clk),
-        .reset(reset),
+        .rst(rst),
         .total(total),
-        .vendA(vendA),
-        .vendB(vendB),
+        .seleccion(seleccion),
+        .producto(producto),
+        .listo(listo),
         .cambio(cambio)
     );
 
 endmodule
+
