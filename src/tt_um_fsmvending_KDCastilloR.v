abd057b885b7f18bcd1c6f345761b4a1617ed7dd
KDCastilloR <kcastillor13@miumg.edu.gt>
2025-06-01 18:08:21 -0600

    Update tt_um_fsmvending_KDCastilloR.v

--- src/tt_um_fsmvending_KDCastilloR.v
+++ src/tt_um_fsmvending_KDCastilloR.v
@@ -1,4 +1,4 @@
-
+`default_nettype none
 `timescale 1ns / 1ps
 
 module tt_um_fsmvending_KDCastilloR (
@@ -13,23 +13,19 @@ module tt_um_fsmvending_KDCastilloR (
 );
 
   wire reset = ~rst_n;
+
   reg [24:0] clk_div;
-  wire clk_slow;
+  always @(posedge clk) clk_div <= clk_div + 1;
+  wire clk_slow = clk_div[24];
 
-  always @(posedge clk) begin
-    clk_div <= clk_div + 1;
-  end
-
-  assign clk_slow = clk_div[24];
-
-  wire [1:0] moneda     = ui_in[1:0];
-  wire [1:0] seleccion  = ui_in[3:2];
+  wire [1:0] moneda = ui_in[1:0];
+  wire [1:0] seleccion = ui_in[3:2];
 
   wire [1:0] producto;
   wire listo;
   wire [1:0] cambio;
 
-  top_maquina vending (
+  top_maquina maquina (
     .clk(clk_slow),
     .rst(reset),
     .moneda(moneda),
@@ -39,9 +35,12 @@ module tt_um_fsmvending_KDCastilloR (
     .cambio(cambio)
   );
 
+  // Empaquetar salida 8 bits:
+  // [7:5]=0, [4]=listo, [3:2]=cambio, [1:0]=producto
   assign uo_out = {3'b000, listo, cambio, producto};
-  assign uio_out = 8'b00000000;
-  assign uio_oe  = 8'b00000000;
+  assign uio_out = 8'b0;
+  assign uio_oe  = 8'b0;
 
 endmodule
+
 
